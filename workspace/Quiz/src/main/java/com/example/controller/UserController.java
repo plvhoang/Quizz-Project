dc8a7af832d471219204fe2b735010393df5d19c
plvhoang <83583063+plvhoang@users.noreply.github.com>
2021-08-24 20:31:45 +0700

    doquiz (chua xong)

--- workspace/Quiz/src/main/java/com/example/controller/UserController.java
+++ workspace/Quiz/src/main/java/com/example/controller/UserController.java
@@ -7,6 +7,13 @@
 
 package com.example.controller;
 
-public class UserController {
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
+public class UserController {
+	@RequestMapping(value={"/"},method = RequestMethod.GET )
+    public String viewLogin(Model model){
+        return "index";
+    }
 }
