088411c1e7c095494330a6b374a94b554acfd3c0
truongn843 <truongn843@gmail.com>
2021-08-26 08:01:07 +0700

    Update login
    fix conflict

---
+++ workspace/Quiz/src/main/java/com/example/entity/PersistentLogins.java
@@ -0,0 +1,58 @@
+package com.example.entity;
+
+import java.sql.Timestamp;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "persistent_logins")
+public class PersistentLogins {
+	@Column(name = "username", length = 64)
+	private String username;
+	
+	@Id
+	@Column(name = "series", length = 64)
+	private String series;
+	
+	@Column(name = "token", length = 64)
+	private String token;
+	
+	@Column(name = "last_used")
+	private Timestamp last_used;
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getSeries() {
+		return series;
+	}
+
+	public void setSeries(String series) {
+		this.series = series;
+	}
+
+	public String getToken() {
+		return token;
+	}
+
+	public void setToken(String token) {
+		this.token = token;
+	}
+
+	public Timestamp getLast_used() {
+		return last_used;
+	}
+
+	public void setLast_used(Timestamp last_used) {
+		this.last_used = last_used;
+	}
+
+}
